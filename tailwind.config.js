4e678dc98bfe0d1880548c4248248b6ea3f7107c
daominh89 <daonhatminh1978@gmail.com>
2022-02-05 12:47:22 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  purge: ["./src/**/*.{vue,js,ts,jsx,tsx}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
