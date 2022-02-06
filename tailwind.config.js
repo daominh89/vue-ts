be09bde34d98643e1be416740794bbc18e6beb19
daominh89 <daonhatminh1978@gmail.com>
2022-02-06 09:27:42 +0700

    replaced vue with vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  purge: ["./src/**/*.{vue,js,ts,jsx,tsx}", "./index.html"],
+  purge: ["src/**/*.{js,ts,jsx,tsx}", "./index.html"],
+  darkMode: true, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
