c675fa4afe2c29869ee59e0b37fd8b27a985a5c9
daominh89 <daonhatminh1978@gmail.com>
2022-02-06 22:54:33 +0700

    Workin on it

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,43 @@
 <template>
-  <div class="container">
+  <nav class="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">
+      <span class="font-bold text-xl">Bruh</span>
+    </div>
+
+    <div class="w-full block flex-grow sm:flex sm:items-center sm:w-auto">
+      <div class="text-sm sm:flex-grow">
+        <a
+          href="#"
+          class="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4"
+        >
+          bruh
+        </a>
+        <a
+          href="#"
+          class="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4"
+        >
+          bruh
+        </a>
+        <a
+          href="#"
+          class="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white"
+        >
+          bruh
+        </a>
+      </div>
+      <div>
+        <a
+          href="#"
+          class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 md:mt-0"
+          >bruh</a
+        >
+      </div>
+    </div>
+  </nav>
+
+  <!-- <div class="container">
     <button
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-full"
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-3xl"
     >
       Find what
     </button>
@@ -23,7 +59,7 @@
       <h4 class="text-xl text-grey-200 font-bold">Vue said:</h4>
       <p class="text-base text-grey-600">You've got mail!</p>
     </div>
-  </div>
+  </div> -->
   <div class="container mt-5 mx-auto px-2">
     <!-- Flex on med screens and up -->
     <div class="md:flex">
